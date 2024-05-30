b5e24eb91b63ae75afe56f65b41e07ad5744eb87
Minadora20 <mukbanianigiviko21@gmail.com>
2024-05-31 01:22:23 +0400

    karoche

--- giviluka/src/app/app.routes.ts
+++ giviluka/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    title: 'Home',
+    loadComponent: () => import('./features/home/home.component'),
+  }];
