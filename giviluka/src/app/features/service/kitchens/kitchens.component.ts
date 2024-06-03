d90728e3e064013ce9393202adf4bb94cbfef4a8
luka <adamialuka23@gmail.com>
2024-06-03 21:18:23 +0400

    kitchennn

--- giviluka/src/app/features/service/kitchens/kitchens.component.ts
+++ giviluka/src/app/features/service/kitchens/kitchens.component.ts
@@ -1,13 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 
+ 
 @Component({
   selector: 'app-kitchens',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, ],
   templateUrl: './kitchens.component.html',
-  styleUrl: './kitchens.component.css'
+  styleUrl: './kitchens.component.scss'
 })
 export class KitchensComponent {
-
 }
