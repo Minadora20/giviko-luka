e7fdd0b0527cfb5b4fd7763aa543a7bcfb66d53c
luka <adamialuka23@gmail.com>
2024-06-06 16:39:49 +0400

    deleted warrings

--- giviluka/src/app/shared/pipes/text-shorter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextShorterPipe } from './text-shorter.pipe';
-
-describe('TextShorterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TextShorterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
